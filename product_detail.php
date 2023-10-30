f77f7b10854952cd40cfafa95994ca38df029f4c
Tu Thi Hue <92870027+Smililyy@users.noreply.github.com>
2023-10-30 23:13:03 +0700

    breakcumb nav

---
+++ product_detail.php
@@ -0,0 +1,21 @@
+<?php include("header.php")?>
+<!-- Main content: start -->
+<div class="breaknav">
+    <ul class="breadcrumb">
+        <li><a href="home.php">Home</a></li>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path d="M6 15L11 10L6 5L7 3L14 10L7 17L6 15Z" fill="black"/>
+            </svg>
+        </li>
+        <li><a href="shop.php">Shop</a></li>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path d="M6 15L11 10L6 5L7 3L14 10L7 17L6 15Z" fill="black"/>
+            </svg>
+        </li>
+        <li><img src="img/Line 5.png" alt=""></li>
+        <li><p>Asgaard sofa</p></li>        
+    </ul>
+    
+</div>
+<!-- Main content: end -->
+<?php include("footer.php")?>
