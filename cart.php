debb9fc795643a9978cbae5c3d71c9e6b8ecbb94
Tu Thi Hue <92870027+Smililyy@users.noreply.github.com>
2023-10-30 12:34:49 +0700

    first commit

---
+++ cart.php
@@ -0,0 +1,4 @@
+<?php include("header.php")?>
+<!-- Main content: start -->
+<!-- Main content: end -->
+<?php include("footer.php")?>
---
+++ home.php
@@ -0,0 +1,8 @@
+<?php include("header.php")?>
+<!-- Main content: start -->
+<div class="banner">
+    <img src="img/banner.jpg"/>
+</div>
+
+<!-- Main content: end -->
+<?php include("footer.php")?>
