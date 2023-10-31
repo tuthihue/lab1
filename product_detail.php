9f7b8a85d0e9d28fd880dbc586bf90809aee2e5b
Tu Thi Hue <92870027+Smililyy@users.noreply.github.com>
2023-10-31 12:45:05 +0700

    fisrt layout product detail

--- product_detail.php
+++ product_detail.php
@@ -15,7 +15,60 @@
         <li><img src="img/Line 5.png" alt=""></li>
         <li><p>Asgaard sofa</p></li>        
     </ul>
-    
+</div>
+<div class="product-information">
+    <div class="col-1">
+        <ul>
+            <li><img src="img/Stuart sofa 1.png" alt=""></li>
+            <li><img src="img/Outdoor sofa set_2 1.png" alt=""></li>
+            <li><img src="img/Outdoor sofa set 2.png" alt=""></li>
+            <li><img src="img/Maya sofa three seater (1) 1.png" alt=""></li>    
+        </ul>
+    </div>
+    <div class="col-4">
+        <div class="main-picture">
+            <img src="img/Asgaard sofa 3.png" alt="">
+        </div>
+    </div>
+    <div class="col-7">
+        <div class="product-inf">
+            <div class="product-inf-name">Asgaard sofa</div>
+            <div class="product-inf-category">Rs. 20,000.00</div>
+            <div class="evaluate">
+                <span class="star">
+                    <img src="img/dashicons_star-filled.png" alt="">
+                    <img src="img/dashicons_star-filled.png" alt="">
+                    <img src="img/dashicons_star-filled.png" alt="">
+                    <img src="img/dashicons_star-filled.png" alt="">
+                    <img src="img/carbon_star-half.png" alt="">
+                </span>
+                <span class="star-line"><img src="img/Line 6.png" alt=""></span>
+                <span class="star-des">5 Customer Review</span>
+            </div>
+            <div class="product-inf-des">
+            Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a
+            well-balanced audio which boasts a clear midrange and extended highs for a sound.
+           </div>
+           <div class="product-inf-size">Size</div>
+           <div class="size">
+                <span class="size1">L</span>
+                <span class="size2">XL</span>
+                <span class="size3">XS</span>
+           </div>
+           <div class="product-inf-color">Color</div>
+           <div class="color">
+                <span>
+                    <img src="img/Rectangle 42.png" alt="">
+                </span>
+                <span>
+                    <img src="img/Rectangle 43.png" alt="">
+                </span>
+                <span>
+                    <img src="img/Rectangle 44.png" alt="">
+                </span>
+           </div>
+        </div>
+    </div>
 </div>
 <!-- Main content: end -->
 <?php include("footer.php")?>
